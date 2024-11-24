cfd2b219fd5b641555ca01cb2363919d5201a08c
Amalsha Fernando <it20055158@my.sliit.lk>
2024-11-25 02:42:46 +0530

    ci.yml file added

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+# tests/test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_get_users(client):
+    response = client.get('/greet/')
+    assert response.status_code == 200
